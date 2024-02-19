5f2551ae26ea04d3d90e9c97b889c4be9ae70f42
Alexis <emacedop@unsa.edu.pe>
2024-02-18 23:59:28 -0500

    PortfolioDEV

---
+++ src/content/config.ts
@@ -0,0 +1,16 @@
+import { z, defineCollection } from 'astro:content';
+
+const projectsCollection = defineCollection({
+	schema: z.object({
+		inProgress: z.boolean(),
+		title: z.string(),
+		description: z.string(),
+		tags: z.array(z.string()),
+		link: z.string(),
+		img_alt: z.string().optional(),
+	}),
+});
+
+export const collections = {
+	'projects': projectsCollection,
+};
